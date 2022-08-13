09454eb78464337e9e8f1ec0e04f86cbe3fbaba2
mfrancavigliaeximie <francaviglia.m@node.coop>
2022-08-13 15:14:23 +0200

    Fine alberatura nodo singolo

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,14 +9,15 @@ use Illuminate\Support\Collection;
 
 class PostsController extends Controller
 {
-    public function populate(Request $request, Response $response) {
+    public function populate(Request $request, Response $response)
+    {
         $ids = [];
         for ($i = 0; $i < 300; $i++) {
             $posts = Post::all();
             $post = new Post;
             $post->text = sha1(uniqid());
             if ($posts) {
-                $ids = $posts->map(function($post) {
+                $ids = $posts->map(function ($post) {
                     return $post->id;
                 });
 
@@ -30,7 +31,8 @@ class PostsController extends Controller
         return \response()->json($ids);
     }
 
-    public function index(Request $request, Response $response) {
+    public function index(Request $request, Response $response)
+    {
         return \response()->json(Post::all()->map(function ($item) {
             if ($item->parent_id === 0) {
                 $item->parent_id = null;
@@ -39,23 +41,24 @@ class PostsController extends Controller
         })->toArray(), 200);
     }
 
-    public function tree() {
+    public function tree()
+    {
         $posts = Post::all()->transform(function ($item) {
             if ($item->parent_id === 0) {
                 $item->parent_id = null;
             }
             return $item;
         })->toArray();
-        $node = $this->getNode($posts, 224);
-        $this->show($node);
-//        return \response()->json($this->getNode($posts, 224));
+
+        return \response()->json($this->getNode($posts, 224));
     }
 
-    private function getNode(array &$array, $id, $parentIdField = 'parent_id', $idField = 'id', $childrenField = 'children', $node = null, $firstIteration = true) {
+    private function getNode(array &$array, $id, $parentIdField = 'parent_id', $idField = 'id', $childrenField = 'children', $node = null, $firstIteration = false)
+    {
         $collection = new Collection($array);
         $nodeKey = 0;
         if (!$node) {
-            $node = $collection->first(function($item, $key) use ($idField, $id, &$nodeKey) {
+            $node = $collection->first(function ($item, $key) use ($idField, $id, &$nodeKey) {
                 $nodeKey = $key;
                 return $item[$idField] === $id;
             });
@@ -63,30 +66,26 @@ class PostsController extends Controller
         }
 
 
-        [$children, $array] = $collection->partition(function($item) use ($parentIdField, $id) {
+        [$children, $array] = $collection->partition(function ($item) use ($parentIdField, $id) {
             return $item[$parentIdField] === $id;
         });
 
 
         $children = array_values($children->toArray());
         $array = array_values($array->toArray());
-        $node[$childrenField] = $children;
 
-//        if (!$firstIteration) {
-//            $this->show($node);
-//        }
-
-        (new Collection($children))->map(function($n) use (&$array, $parentIdField, $idField, $childrenField, $node) {
+        $node[$childrenField] = (new Collection($children))->map(function ($n) use (&$array, $parentIdField, $idField, $childrenField, $node, $firstIteration) {
             $n = $this->getNode($array, $n[$idField], $parentIdField, $idField, $childrenField, $n, false);
             return $n;
-        });
+        })->toArray();
         return $node;
     }
 
-    public function show($data) {
+    public function show($data)
+    {
         echo '<pre>';
         print_r($data);
         echo '</pre>';
-        die();
+//        die();
     }
 }
